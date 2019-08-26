c7e4e5bb22a346f07e623692b335073c6212abc4
Varun Sharma <varunvsharma@gmail.com>
2019-08-26 23:17:42 +1000

    Feature: Add StageToRedshiftOperator

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,24 +1,52 @@
 from airflow.hooks.postgres_hook import PostgresHook
+from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
+    template_fields = ('s3_key',)
 
     @apply_defaults
-    def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
+    def __init__(self,              
+                 redshift_conn_id="",
+                 aws_credentials_id="",
+                 table="",
+                 s3_bucket="",
+                 s3_key="",
+                 delimiter=",",
+                 ignore_headers=1,
+                 data_format="",
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.aws_credentials_id = aws_credentials_id
+        self.table = table
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.delimiter = delimiter
+        self.ignore_headers = ignore_headers
+        self.data_format = data_format
 
     def execute(self, context):
         self.log.info('StageToRedshiftOperator not implemented yet')
+        aws_hook = AwsHook(self.aws_credentials_id)
+        credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        self.log.info('Clearing data from destination table')
+        redshift.run(f'DELETE FROM {self.table}')
+
+        self.log.info('Copying data from S3 to Redshift')
+        rendered_key = self.s3_key.format(**context)
+        s3_path = f's3://{self.s3_bucket}/{rendered_key}'
+        formatted_sql = f"""COPY {self.table}
+                            FROM {s3_path}
+                            FORMAT AS {self.data_format} 'auto'
+                            ACCESS_KEY_ID '{credentials.access_key}'
+                            SECRET_ACCESS_KEY '{credentials.secret_access_key}';
+                            """
 
 
 
