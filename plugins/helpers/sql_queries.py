4588fc10c87a8f28d0d1dbd9bd21789c0b9a93cc
Varun Sharma <varunvsharma@gmail.com>
2019-08-27 00:50:14 +1000

    Refactor: Add create table queries to SQL helper file

--- plugins/helpers/sql_queries.py
+++ plugins/helpers/sql_queries.py
@@ -40,3 +40,90 @@ class SqlQueries:
                extract(month from start_time), extract(year from start_time), extract(dayofweek from start_time)
         FROM songplays
     """)
+
+    staging_events_table_create = ("""CREATE TABLE IF NOT EXISTS staging_events (artist varchar,
+                                                                                 auth varchar,
+                                                                                 firstname varchar,
+                                                                                 gender varchar,
+                                                                                 iteminsession int,
+                                                                                 lastname varchar,
+                                                                                 length float,
+                                                                                 level varchar,
+                                                                                 location varchar,
+                                                                                 method varchar,
+                                                                                 page varchar,
+                                                                                 registration float,
+                                                                                 sessionid int,
+                                                                                 song varchar,
+                                                                                 status int,
+                                                                                 ts timestamp,
+                                                                                 useragent varchar,
+                                                                                 userid int)
+    """)
+
+    staging_songs_table_create = ("""CREATE TABLE IF NOT EXISTS staging_songs (num_songs int,
+                                                                               artist_id varchar(18),
+                                                                               artist_latitude float,
+                                                                               artist_longitude float,
+                                                                               artist_location varchar,
+                                                                               artist_name varchar,
+                                                                               song_id varchar(18),
+                                                                               title varchar,
+                                                                               duration float,
+                                                                               year int)
+    """)
+
+    
+    songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplay (songplay_id varchar(32),
+                                                                     start_time timestamp NOT NULL,
+                                                                     userid int NOT NULL,
+                                                                     level varchar,
+                                                                     song_id varchar(18) NOT NULL DISTKEY,
+                                                                     artist_id varchar(18) NOT NULL,
+                                                                     sessionid int,
+                                                                     location varchar,
+                                                                     useragent varchar,
+                                                                     PRIMARY KEY (songplay_id))
+    """)
+
+    user_table_create = ("""CREATE TABLE IF NOT EXISTS users (userid int,
+                                                              firstname varchar,
+                                                              lastname varchar,
+                                                              gender varchar,
+                                                              level varchar,
+                                                              PRIMARY KEY (user_id))
+                                                              DISTSTYLE AUTO
+    """)
+
+    song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id varchar(18) DISTKEY,
+                                                              title varchar,
+                                                              artist_id varchar(18) NOT NULL,
+                                                              year int,
+                                                              duration float,
+                                                              PRIMARY KEY (song_id))
+                                                              DISTSTYLE KEY
+    """)
+
+    artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id varchar(18),
+                                                                  artist_name varchar,
+                                                                  artist_location varchar,
+                                                                  artist_latitude float,
+                                                                  artist_longitude float,
+                                                                  PRIMARY KEY (artist_id))
+                                                                  DISTSTYLE AUTO
+    """)
+
+    time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time timestamp,
+                                                             hour int,
+                                                             day int,
+                                                             week int,
+                                                             month int,
+                                                             year int,
+                                                             weekday int,
+                                                             PRIMARY KEY (start_time))
+                                                             DISTSTYLE AUTO
+    """)
+
+
+
+
