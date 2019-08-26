331e975a9c2ab540c81dfdf03183e01b5dbd817d
Varun Sharma <varunvsharma@gmail.com>
2019-08-26 23:30:41 +1000

    Chore: Configure staging tasks

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -28,12 +28,24 @@ start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
-    dag=dag
+    dag=dag,
+    redshift_conn_id='redshift',
+    aws_credentials_id='aws_credentials',
+    table='staging_events',
+    s3_bucket='udacity-dend',
+    s3_key='log_data',
+    data_format='json'
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='Stage_songs',
-    dag=dag
+    dag=dag,
+    redshift_conn_id='redshift',
+    aws_credentials_id='aws_credentials',
+    table='staging_songs',
+    s3_bucket='udacity-dend',
+    s3_key='song_data',
+    data_format='json'
 )
 
 load_songplays_table = LoadFactOperator(
