6fdca0dd9f36add3211ef9594b9a416a7390ab7c
Varun Sharma <varunvsharma@gmail.com>
2019-08-28 00:29:21 +1000

    Feature: Add LoadDimensionOperator

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -8,15 +8,31 @@ class LoadDimensionOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 create_table_sql="",
+                 table="",
+                 values="",
+                 insert_mode="truncate",
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.create_table_sql = create_table_sql
+        self.table = table
+        self.values = values
+        self.insert_mode = insert_mode
 
     def execute(self, context):
         self.log.info('LoadDimensionOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        if self.create_table_sql:
+            self.log.info(f'Creating {self.table} table if it does not exist')
+            redshift.run(self.create_table_sql)
+
+        if self.insert_mode == 'truncate':
+            self.log.info(f'Deleting all rows from {self.table}')
+            redshift.run(f'TRUNCATE {self.table}')
+
+        self.log.info('Inserting data into dimension table')
+        reshift.run(f'INSERT INTO {self.table} {self.values}')
